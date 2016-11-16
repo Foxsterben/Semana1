b2cd74c99c4411bbc83fac92595b19a0ec89d09b
Foxsterben <ss.baez@comunidad.unam.mx>
2016-11-15 23:42:12 -0600

    ey!

--- app/src/main/java/com/ss_baez/petagram/MainActivity.java
+++ app/src/main/java/com/ss_baez/petagram/MainActivity.java
@@ -1,13 +1,68 @@
 package com.ss_baez.petagram;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout sfiIndicadorRefresh;
+    ListView lstMiLista;
+    Adapter adaptador;//Sirve como aux para manejar todos los datos del archivo strings.xml y pasarlos a la ListView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFAB();
+
+        sfiIndicadorRefresh = (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMiLista = (ListView) findViewById(R.id.lstMiLista);
+
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+
+        sfiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refrescandoContenido();
+            }
+        });
+
+    }
+
+    public void refrescandoContenido(){ //Aquí se pude hacer que la app se conecte a un web service y traiga nuevos datos etc
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+        sfiIndicadorRefresh.setRefreshing(false);
+    }
+
+    public void agregarFAB(){
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFAB);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, getResources().getString(R.string.mensaje), Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("Snackbar", "Click en Snack Bar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
+
+            }
+        });
     }
 }
